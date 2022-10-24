01f25acd5877d91007220297a15f12fe548ff041
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 01:00:21 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string hello = "Hello World";
+
+Console.WriteLine($"Merhaba de babana {hello}");
+
+
+string firtsfri = "Murat Han";
+string secondfri = "Suat";
+
+Console.WriteLine($"İki kardeşin adları {firtsfri} ve {secondfri}");
